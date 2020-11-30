a072c76949b54fb68bb6d07ef2cfc56abd924b2f
TehcJS <tehcdev@gmail.com>
2020-11-30 00:51:06 -0500

    concen

--- src/main/java/io/github/tehcjs/fabricpvp/modules/Module.java
+++ src/main/java/io/github/tehcjs/fabricpvp/modules/Module.java
@@ -9,20 +9,7 @@ import java.util.List;
 import static io.github.tehcjs.fabricpvp.utilities.Util.trans;
 
 public abstract class Module {
-	private static Module INSTANCE;
 	private static final List<Module> MODULES = new ArrayList<>();
-
-	// initialize every module
-	static {
-		for (Class<?> c : Module.class.getClasses()) {
-			try {
-				INSTANCE = (Module) c.newInstance();
-				MODULES.add(INSTANCE);
-			} catch (IllegalAccessException | InstantiationException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 
 	public TranslatableText getName() {
 		return trans(this.getId().getPath());
@@ -30,15 +17,4 @@ public abstract class Module {
 
 	public abstract Identifier getId();
 	public abstract void register();
-
-	public static Module getInstance() {
-		if (INSTANCE != null) return INSTANCE;
-
-		try {
-			return INSTANCE = Module.class.newInstance();
-		} catch (IllegalAccessException | InstantiationException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
 }
--- src/main/java/io/github/tehcjs/fabricpvp/modules/optimizations/chunk/ChunkOptimizationsModule.java
+++ src/main/java/io/github/tehcjs/fabricpvp/modules/optimizations/chunk/ChunkOptimizationsModule.java
@@ -6,6 +6,8 @@ import net.minecraft.util.Identifier;
 import static io.github.tehcjs.fabricpvp.utilities.Util.id;
 
 public class ChunkOptimizationsModule extends Module {
+	public static final Module INSTANCE = new ChunkOptimizationsModule();
+
 	@Override
 	public Identifier getId() {
 		return id("chunk_optimizations");
